b8bd0978a56c599e7d28b90c66115cfffd91c548
Geran <geranstortelers@gmail.com>
2022-11-25 12:15:08 +0100

    Add project files.

---
+++ Icarus/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Discord;
+
+namespace Icarus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
