c705331254747b5ac53ec2fd8afe652f474ceedb
GeranS <geranstortelers@gmail.com>
2023-04-27 22:27:08 +0200

    :) 2

--- Icarus/Services/ValueManagementService.cs
+++ Icarus/Services/ValueManagementService.cs
@@ -221,7 +221,7 @@ namespace Icarus.Services
         {
             using var db = new IcarusContext();
 
-            string DataPath = "../../ValueRelationShips.xml";
+            string DataPath = "../ValueRelationShips.xml";
             XmlDocument Xmldata = new XmlDocument();
             Xmldata.Load(DataPath);
             List<RelationShipDTO> RelationShipDTOs = new List<RelationShipDTO>();
