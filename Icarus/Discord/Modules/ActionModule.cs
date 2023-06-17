40cf748490ec32dc478d7c4cd84b2eca73fd7c19
Geran <geranstortelers@gmail.com>
2023-06-17 20:14:44 +0200

    Added command for giving favours

--- Icarus/Discord/Modules/ActionModule.cs
+++ Icarus/Discord/Modules/ActionModule.cs
@@ -1,8 +1,11 @@
 ﻿using Discord.Interactions;
+using Discord.Interactions.Builders;
 using Discord.WebSocket;
 using Icarus.Context;
+using Icarus.Context.Models;
 using Icarus.Discord.CustomPreconditions;
 using Icarus.Services;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +18,13 @@ namespace Icarus.Discord.Modules
     {
         private readonly DiscordSocketClient _client;
         private readonly ActionService _actionService;
+        private readonly DiscordInteractionHelpers _interactionHelpers;
 
-        public ActionModule(DiscordSocketClient client, ActionService actionService)
+        public ActionModule(DiscordSocketClient client, ActionService actionService, DiscordInteractionHelpers interactionHelpers)
         {
             _client = client;
             _actionService = actionService;
+            _interactionHelpers = interactionHelpers;
         }
 
 
@@ -43,10 +48,44 @@ namespace Icarus.Discord.Modules
         [SlashCommand("give-favour", "Gives favours")]
         [RequireProfile]
         [RequireAdmin]
-        public async Task GiveToken(int amount, SocketGuildUser mention,
-            [Choice("Test Token", ActionTokenType.TestToken.ToString()), Choice(), Choice()] string tokenType)
+        public async Task GiveToken(SocketGuildUser mention, ActionTokenType tokenType, int amount)
         {
+            if (amount > 7)
+            {
+                await RespondAsync("May not give a character more than seven favours of any type.");
+                return;
+            }
 
+            using var db = new IcarusContext();
+
+            var character = db.Characters.Include(c => c.Tokens)
+                .FirstOrDefault(c => c.DiscordUserId == Context.User.Id.ToString() && c.YearOfDeath != -1);
+
+            if (character == null)
+            {
+                await RespondAsync($"User {Context.User.Username} does not have an active character.");
+                return;
+            }
+
+            var tokenEntry = character.Tokens.FirstOrDefault(t => t.TokenType == tokenType);
+
+            if (tokenEntry == null)
+            {
+                var newTokenEntry = new CharacterToken()
+                {
+                    PlayerCharacterId = character.CharacterId,
+                    TokenType = tokenType,
+                    Amount = amount
+                };
+
+                character.Tokens.Add(newTokenEntry);
+
+                tokenEntry = newTokenEntry;
+            }
+
+            if (tokenEntry.Amount > 7) tokenEntry.Amount =7;
+
+            await RespondAsync("Added favours.");
         }
     }
 }
