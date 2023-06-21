3d8448562cfb7892504bc10d1ef162136c07086e
GeranS <geranstortelers@gmail.com>
2023-06-21 23:00:21 +0200

    added graveyard add command

--- Icarus/Discord/Modules/GraveyardModule.cs
+++ Icarus/Discord/Modules/GraveyardModule.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.Interactions;
 using Icarus.Discord.CustomPreconditions;
+using Icarus.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,23 @@ using System.Threading.Tasks;
 
 namespace Icarus.Discord.Modules
 {
-    public class GraveyardModule
+    public class GraveyardModule : InteractionModuleBase<SocketInteractionContext>
     {
-        public GraveyardModule() { }
+        private readonly GraveyardService _graveyardService;
+
+        public GraveyardModule(GraveyardService graveyardService) 
+        {
+            _graveyardService = graveyardService;
+        }
 
         [SlashCommand("add-graveyard", "Adds the channel as a graveyard.")]
         public async Task AddGraveyard()
         {
+            var channelId = Context.Channel.Id;
 
+            var result = await _graveyardService.AddGraveyardChannel(channelId);
+
+            await RespondAsync(result);
         }
     }
 }
